e04b2c3468921b2e00f1afec054d7405cffd5689
hitarthdesai17 <hitarthdesai184@gmail.com>
2026-06-26 14:51:44 +0530

    The images and text will upload in user_uploads by itself

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template , request
-import uuid
+import uuid , os
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+UPLOAD_FOLDER = 'user_uploads'
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route("/")
 def home():
@@ -8,11 +14,22 @@ def home():
 
 @app.route("/create",methods=["GET","POST"])
 def create():
-    myid = uuid.uuid1()
+    myid = str(uuid.uuid1())
     if request.method=="POST":
         print(request.files.keys())
-        for key,value in request.files.items():
-            print(key,value)
+        rec_id=request.form.get("uuid")
+        desc=request.form.get("text")
+        folder = os.path.join(app.config['UPLOAD_FOLDER'],rec_id)
+        os.makedirs(folder,exist_ok=True)
+
+        for file in request.files.values():
+            filename = secure_filename(file.filename) 
+            #Upload File
+            if file and filename:
+                file.save(os.path.join(folder,filename))
+         #Capture the description and save it to file
+        with open(os.path.join(folder, "desc.txt"),"w",encoding="utf-8") as f:
+                f.write(desc)
 
     return render_template("create.html",myid=myid)
 
